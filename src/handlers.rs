25df8009581af9b3fade9f70b9e931df787e4c9a
deciduously <github@deciduously.com>
2018-06-29 13:31:24 -0400

    Broken markdown skeleton - inserts html as plaintext

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,16 +2,15 @@ use actix_web::{Error, HttpRequest, HttpResponse, Path, Responder};
 use askama::Template;
 use markdown::bake;
 
+// this couldn't be a &'a str because of the impl Responder
 #[derive(Template)]
 #[template(path = "index.html")]
-
-// this can be named anything
-struct IndexTemplate<'a> {
-    name: &'a str,
+struct PageWrapperTemplate {
+    page: String,
 }
 
 // TODO This boilerplate feels automatable
-impl<'a> Responder for IndexTemplate<'a> {
+impl Responder for PageWrapperTemplate {
     type Item = HttpResponse;
     type Error = Error;
 
@@ -23,7 +22,9 @@ impl<'a> Responder for IndexTemplate<'a> {
 }
 
 pub fn index(_req: HttpRequest) -> impl Responder {
-    IndexTemplate { name: "world" }
+    PageWrapperTemplate {
+        page: "Index Page".to_string(),
+    }
 }
 
 // BEN - you should be baking these ahead of time for production serving.
@@ -41,8 +42,10 @@ pub fn index(_req: HttpRequest) -> impl Responder {
 // in draft/ it'll happen on the fly
 // in post/ it'll be part of the bake process.
 
-pub fn parse_md(post: Path<String>) -> Result<HttpResponse, Error> {
+// TODO here's part of your problem
+pub fn parse_md(post: Path<String>) -> impl Responder {
     // TODO return a 404 if not found
-    let html = bake(&post)?;
-    Ok(HttpResponse::Ok().content_type("text/html").body(html))
+    PageWrapperTemplate {
+        page: bake(&post).expect("Could not bake requested markdown resource"),
+    }
 }
