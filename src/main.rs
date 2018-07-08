c296dcaa56663e045cfb58a9a12d88f744ec8a2b
deciduously <bendlovy@gmail.com>
2018-07-08 07:48:33 -0400

    file_names()

--- src/main.rs
+++ src/main.rs
@@ -30,34 +30,32 @@ impl Cmd {
     }
 }
 
+fn file_names(path: &str) -> io::Result<Vec<String>> {
+    Ok(fs::read_dir(path)?
+        .filter_map(|file| {
+            file.ok().and_then(|e| {
+                e.path()
+                    .file_name()
+                    .and_then(|n| n.to_str().map(|s| String::from(s)))
+            })
+        })
+        .collect::<Vec<String>>())
+}
+
 fn publish() -> io::Result<()> {
     // pop a brand new child template in posts/
     // for every .md in drafts that doesnt have a match
     // To re-publish, we'll need to specify the specific post name
     // For starters, though, just get this working
 
-    let drafts = fs::read_dir("./drafts/")?;
-    let posts = fs::read_dir("./posts/")?;
-    let mut unpublished = Vec::new();
+    // TODO make /posts/ if it doesn't exist
 
-    // BEN none of this owrked - above yo should use iterators to just collect filenames
+    let drafts = file_names("./drafts/")?;
+    let posts = file_names("./posts/")?;
 
     // Find the drafts with no matching post
-    for draft in drafts {
-        let draft_name = draft.unwrap().file_name();
-        let mut unpublished_draft = true;
-        for post in posts {
-            if draft_name == post.unwrap().file_name() {
-                unpublished_draft == false;
-                break;
-            }
-        }
-        if unpublished_draft {
-            unpublished.push(&draft);
-        }
-    }
 
-    println!("Unpublished: {:?}", unpublished);
+    println!("Drafts: {:?}\nPosts: {:?}", drafts, posts);
     Ok(())
 }
 
