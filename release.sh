937d3ab5cbe8844c5613cbc91b6939437e1721a0
deciduously <ben@deciduously.com>
2018-09-09 22:42:00 -0400

    change release to docker

--- release.sh
+++ release.sh
@@ -1,18 +1,8 @@
 #/usr/bin/env sh
 
-OUT="release/"
+VERSION="0.2.0"
 
-# build executabe
-cargo build --release
-
-# purge release dir
-if [ -d "$OUT" ]; then
-    rm -rf $OUT
-fi
-mkdir $OUT
-
-# copy everything in
-cp LICENSE $OUT
-cp README.md $OUT
-cp -r templates $OUT
-cp target/release/deciduously-com $OUT
+cargo clean
+docker build -t deciduously-com .
+docker tag deciduously-com deciduously0/deciduously-com:$VERSION
+docker push deciduously0/deciduously-com:$VERSION
